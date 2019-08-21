79114fc7bfdddb86cfa25c4df2a87b1854d81224
楊昀羲 <ray650128@gmail.com>
2019-08-21 15:19:21 +0800

    實作範例

--- app/src/main/java/com/ray650128/circleprogressbardemo/MainActivity.kt
+++ app/src/main/java/com/ray650128/circleprogressbardemo/MainActivity.kt
@@ -2,11 +2,26 @@ package com.ray650128.circleprogressbardemo
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SeekBar
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                if(fromUser) {
+                    circleProgressBar.setCurrentValues(progress.toFloat())
+                }
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+
+        })
     }
 }
